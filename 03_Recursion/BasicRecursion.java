2540fb29835bd9117bdfa06e37d4fa643d19a9db
riteshsaini01 <b210885@skit.ac.in>
2023-08-11 10:02:04 +0530

    Adding codes

---
+++ 03_Recursion/BasicRecursion.java
@@ -0,0 +1,21 @@
+class BasicRecursion
+{
+    static void fun1()
+    {
+        System.out.println("fun1");
+    }
+    static void fun2()
+    {
+        System.out.println("Before fun1");
+        fun1();
+        System.out.println("After fun1");
+    } 
+
+    public static void main(String args[])
+    {
+        System.out.println("Before fun2");
+        fun2();
+        System.out.println("After fun2");
+    }
+
+}
