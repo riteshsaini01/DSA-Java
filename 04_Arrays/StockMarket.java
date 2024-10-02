6a6de75a4c14f42dc405d62e107809a46e527b58
riteshsaini01 <b210885@skit.ac.in>
2024-10-02 16:18:45 +0530

    added some functionalities

---
+++ 04_Arrays/StockMarket.java
@@ -0,0 +1,27 @@
+public class StockMarket {
+
+    static int maxProfit(int price[], int start, int end) {
+        if (end <= start)
+            return 0;
+
+        int profit = 0;
+        for (int i = start; i < end; i++) {
+            for (int j = i + 1; j <= end; j++) {
+                if (price[j] > price[i]) {
+                    int curr_profit = price[j] - price[i] + maxProfit(price, start, i - 1) + maxProfit(price, j + 1, end);
+
+                    profit = Math.max(profit, curr_profit);
+                }
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String args[]) {
+        int price[] = { 1, 5, 3, 8, 12 };
+        int start = 0;
+        int end = 4;
+
+        System.out.println(maxProfit(price, start, end));
+    }
+}
